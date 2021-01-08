dd8124d1e8fcd38a4e6abd7dbae081aa985a7192
Corgniard <antoine.corgniard@gmail.com>
2021-01-08 14:00:42 +0100

    a beautiful level design

---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    public float ballSpeed;
+    public float jumpVelocity = 5f;
+    public LayerMask groundLayer;
+    public float distanceToGround = 0.1f;
+
+
+    private Rigidbody _rb;
+    private SphereCollider _col;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xSpeed = Input.GetAxis("Horizontal");
+        float ySpeed = Input.GetAxis("Vertical");
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddTorque(new Vector3(xSpeed, 0, ySpeed) * ballSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 sphereBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckSphere(sphereBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+        Debug.Log(grounded);
+        Debug.Log(sphereBottom);
+        return grounded;
+    }
+}
