4b89ceeb49d37d55bae74bbd80a7e6de3f162e07
Ekode <ekode@protonmail.com>
2021-01-08 12:52:31 +0100

    Added success UI and UI Manager

---
+++ Assets/Scripts/LoadingScene/UIBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIBehaviour : MonoBehaviour
+{
+
+    public void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Retry()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void GoToNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/LoadingScene/UIManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public List<GameObject> prefabsToInst;
+
+    void Start()
+    {
+        foreach(GameObject prefab in prefabsToInst)
+        {
+            GameObject toAdd = Instantiate(prefab);
+            toAdd.name = prefab.name;
+            toAdd.transform.SetParent(transform);
+            toAdd.SetActive(false);
+        }
+        showSuccessUI();
+        showGameOverUI();
+    }
+
+    private void hideAllUI()
+    {
+        this.FindObject("GameOverUI").SetActive(false);
+        this.FindObject("SuccessUI").SetActive(false);
+    }
+
+    public void showGameOverUI()
+    {
+        hideAllUI();
+        GameObject gameOverUi = this.FindObject("GameOverUI");
+        gameOverUi.SetActive(true);
+    }
+
+    public void showSuccessUI()
+    {
+        hideAllUI();
+        GameObject gameOverUi = this.FindObject("SuccessUI");
+        gameOverUi.SetActive(true);
+    }
+
+    private GameObject FindObject(string name)
+    {
+        Transform[] trs = this.GetComponentsInChildren<Transform>(true);
+        foreach (Transform t in trs)
+        {
+            if (t.name == name)
+            {
+                return t.gameObject;
+            }
+        }
+        return null;
+    }
+}
